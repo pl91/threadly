5548902eb27ffddda6807a9f747048afd56e6d3f
Paul Liani <pliani91@gmail.com>
2023-03-07 17:05:10 -0500

    basic routing

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,22 @@
+import "./category-item.styles.scss";
+
+// de-structer from our prop and return content
+const CategoryItem = ({ category }) => {
+    const { imageUrl, title } = category;
+  return (
+    <div className="category-container">
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryItem;
